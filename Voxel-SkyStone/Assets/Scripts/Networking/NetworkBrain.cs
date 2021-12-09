3f0cc26ea6ce8211767fd722b2b4c3f49ae92f14
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-12-09 14:32:40 +0100

    NetworkBrain event fixes over multiple scenes

--- Voxel-SkyStone/Assets/Scripts/Networking/NetworkBrain.cs
+++ Voxel-SkyStone/Assets/Scripts/Networking/NetworkBrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using SimpleJSON;
 using UnityEngine;
@@ -11,7 +12,7 @@ namespace Networking
         [SerializeField] private bool localHost;
         [SerializeField] private bool connectOnAwake = false;
         [SerializeField] private NetworkData networkData;
-        
+
         public UnityEvent onConnectionOpen = new UnityEvent();
         public UnityEvent onConnectionFail = new UnityEvent();
         public UnityEvent onConnectionSuccessful = new UnityEvent();
@@ -21,34 +22,32 @@ namespace Networking
         private void Awake()
         {
             if (connectOnAwake) Connect();
+
+            if (networkData.Client != null)
+            {
+                networkData.Client.OnOpen += OnConnectionOpen;
+                networkData.Client.OnMessage += OnMessage;
+            }
         }
-    
+
+        private void OnDisable()
+        {
+            networkData.Client.OnOpen -= OnConnectionOpen;
+            networkData.Client.OnMessage -= OnMessage;
+        }
+
         public void Connect()
         {
             StartCoroutine(TryConnect());
         }
-    
+
         private IEnumerator TryConnect()
         {
             networkData.Client = localHost ? new WebSocket("ws://localhost:80") : new WebSocket("ws://voxel-relay.herokuapp.com/");
-            networkData.Client.OnMessage += (sender, e) =>
-            {
-                JSONNode jsonNode = JSONNode.Parse(e.Data);
-                if (jsonNode["action"] == NetworkAction.Connected.ToString())
-                {
-                    networkData.MyId = jsonNode["playerId"];
-                }
-                if (jsonNode["action"] == NetworkAction.StartGame.ToString())
-                {
-                    networkData.PlayerTurnId = jsonNode["StartingPlayerId"];
-                    onGameStart?.Invoke();
-                }
-            };
-            networkData.Client.OnOpen += (sender, args) =>
-            {
-                Debug.Log("connected opened");
-                onConnectionOpen?.Invoke();
-            };
+            
+            networkData.Client.OnOpen += OnConnectionOpen;
+            networkData.Client.OnMessage += OnMessage;
+            
             networkData.Client.ConnectAsync();
 
             float waitedTime = 0;
@@ -68,6 +67,26 @@ namespace Networking
             {
                 print("something went wrong while connecting");
                 onConnectionFail?.Invoke();
+            }
+        }
+
+        private void OnConnectionOpen(object sender, EventArgs args)
+        {
+            Debug.Log("connection opened");
+        }
+
+        private void OnMessage(object sender, MessageEventArgs args)
+        {
+            JSONNode jsonNode = JSONNode.Parse(args.Data);
+            if (jsonNode["action"] == NetworkAction.Connected.ToString())
+            {
+                networkData.MyId = jsonNode["playerId"];
+            }
+
+            if (jsonNode["action"] == NetworkAction.StartGame.ToString())
+            {
+                networkData.PlayerTurnId = jsonNode["StartingPlayerId"];
+                onGameStart?.Invoke();
             }
         }
     }
