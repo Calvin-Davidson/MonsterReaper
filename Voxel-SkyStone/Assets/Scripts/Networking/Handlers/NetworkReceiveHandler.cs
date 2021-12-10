f45f91ddad45d3025a07ecbc2017ec0e3f73cb51
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-12-10 13:49:13 +0100

    Fixed that you can load scenes when handling a package

---
+++ Voxel-SkyStone/Assets/Scripts/Networking/Handlers/NetworkReceiveHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Networking;
+using SimpleJSON;
+using UnityEngine;
+using WebSocketSharp;
+
+public class NetworkReceiveHandler : MonoBehaviour
+{
+    [SerializeField] private NetworkData networkData;
+    private static NetworkReceiveHandler _instance;
+
+    private void Start()
+    {
+        if (_instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        _instance = this;
+    }
+
+    public void OnPacketReceive(MessageEventArgs args, NetworkData data)
+    {
+        Debug.Log(args.Data);
+        JSONNode jsonNode = JSONNode.Parse(args.Data);
+        string actionName = jsonNode["action"];
+
+        NetworkEventContainer events = NetworkClient.Instance.Events;
+        if (actionName == NetworkAction.Connected.ToString()) events.onConnected?.Invoke(jsonNode);
+        if (actionName == NetworkAction.EndGame.ToString()) events.onGameEnd?.Invoke(jsonNode);
+        if (actionName == NetworkAction.StartGame.ToString()) events.onGameStart?.Invoke(jsonNode);
+        if (actionName == NetworkAction.TurnSwitch.ToString()) events.onTurnSwitch?.Invoke(jsonNode);
+        if (actionName == NetworkAction.PlaceStone.ToString()) events.onStonePlace?.Invoke(jsonNode);
+    } 
+}
--- Voxel-SkyStone/Assets/Scripts/Networking/NetworkEventContainer.cs
+++ Voxel-SkyStone/Assets/Scripts/Networking/NetworkEventContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using SimpleJSON;
 using UnityEngine;
 using UnityEngine.Events;
