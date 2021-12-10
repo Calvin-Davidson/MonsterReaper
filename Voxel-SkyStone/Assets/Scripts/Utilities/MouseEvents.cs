ad9c1f57daf8f018fdc86fd23a2ccb253883e4f4
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-12-10 14:41:49 +0100

    Start menu looks a bit better now

---
+++ Voxel-SkyStone/Assets/Scripts/Utilities/MouseEvents.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onMouseEnter;
+    [SerializeField] private UnityEvent onMouseExit;
+    [SerializeField] private UnityEvent onMouseClick;
+
+    private bool _isMouseOver = false;
+
+    private void OnMouseDown()
+    {
+        onMouseClick?.Invoke();
+    }
+
+    private void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject == this.gameObject && !_isMouseOver)
+            {
+                _isMouseOver = true;
+                onMouseEnter?.Invoke();
+            }
+
+            if (hit.collider.gameObject != this.gameObject && _isMouseOver)
+            {
+                _isMouseOver = false;
+                onMouseExit?.Invoke();
+            }
+        }
+        else if (_isMouseOver)
+        {
+            _isMouseOver = false;
+            onMouseExit?.Invoke();
+        }
+    }
+}
