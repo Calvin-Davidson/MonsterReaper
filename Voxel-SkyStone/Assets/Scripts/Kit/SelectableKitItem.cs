f5cceb8a0622ee3e79a1757e9decef5988734377
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-12-15 20:23:58 +0100

    Rendering the damage points in the kit menu

--- Voxel-SkyStone/Assets/Scripts/Kit/SelectableKitItem.cs
+++ Voxel-SkyStone/Assets/Scripts/Kit/SelectableKitItem.cs
@@ -11,7 +11,14 @@ public class SelectableKitItem : MonoBehaviour
     [SerializeField] private MeshRenderer outlineObject;
     [SerializeField] private MeshRenderer textureObject;
     [SerializeField] private TextMesh priceText;
+    
+    [SerializeField] private GameObject topArrowContainer;
+    [SerializeField] private GameObject rightArrowContainer;
+    [SerializeField] private GameObject bottomArrowContainer;
+    [SerializeField] private GameObject leftArrowContainer;
 
+    [SerializeField] private GameObject[] arrows;
+    
     private bool _isSelected = false;
 
     private Color _defaultColor;
@@ -37,7 +44,24 @@ public class SelectableKitItem : MonoBehaviour
     {
         textureObject.material.mainTexture = data.Texture;
         priceText.text = data.Price.ToString();
+        
+        SpawnArrows(data);
     }
+    
+    private void SpawnArrows(StoneData stoneData)
+    {
+        if (stoneData.TopDamage > 0) SpawnArrow(topArrowContainer, arrows[stoneData.TopDamage-1]);
+        if (stoneData.RightDamage > 0) SpawnArrow(rightArrowContainer, arrows[stoneData.RightDamage-1]);
+        if (stoneData.BottomDamage > 0) SpawnArrow(bottomArrowContainer, arrows[stoneData.BottomDamage-1]);
+        if (stoneData.LeftDamage > 0) SpawnArrow(leftArrowContainer, arrows[stoneData.LeftDamage-1]);
+    }
+    
+    private void SpawnArrow(GameObject container, GameObject arrow)
+    {
+        Instantiate(arrow, container.transform, false);
+    }
+
+
 
     public bool IsSelected => _isSelected;
 }
