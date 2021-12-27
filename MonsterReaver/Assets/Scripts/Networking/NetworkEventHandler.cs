a313c8af364d7f5a7974c40d42d9190de293c38e
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-12-27 11:51:04 +0100

    OpponentLeave event not invoked

--- MonsterReaver/Assets/Scripts/Networking/NetworkEventHandler.cs
+++ MonsterReaver/Assets/Scripts/Networking/NetworkEventHandler.cs
@@ -14,6 +14,7 @@ public class NetworkEventHandler : MonoBehaviour
         NetworkClient.Instance.Events.onGameEnd.AddListener(container.onGameEnd.Invoke);
         NetworkClient.Instance.Events.onStonePlace.AddListener(container.onStonePlace.Invoke);
         NetworkClient.Instance.Events.onTurnSwitch.AddListener(container.onTurnSwitch.Invoke);
+        NetworkClient.Instance.Events.onOpponentLeave.AddListener(container.onOpponentLeave.Invoke);
         NetworkClient.Instance.Events.onGameFound.AddListener(container.onGameFound.Invoke);
         NetworkClient.Instance.Events.onJoinLobby.AddListener(container.onJoinLobby.Invoke);
     }
@@ -24,6 +25,7 @@ public class NetworkEventHandler : MonoBehaviour
         NetworkClient.Instance.Events.onGameEnd.RemoveListener(container.onGameEnd.Invoke);
         NetworkClient.Instance.Events.onStonePlace.RemoveListener(container.onStonePlace.Invoke);
         NetworkClient.Instance.Events.onTurnSwitch.RemoveListener(container.onTurnSwitch.Invoke);
+        NetworkClient.Instance.Events.onOpponentLeave.RemoveListener(container.onOpponentLeave.Invoke);
         NetworkClient.Instance.Events.onGameFound.RemoveListener(container.onGameFound.Invoke);
         NetworkClient.Instance.Events.onJoinLobby.RemoveListener(container.onJoinLobby.Invoke);
     }
